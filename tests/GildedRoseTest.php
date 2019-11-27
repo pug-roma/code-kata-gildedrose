5587ae04aea139bf52a83167247d17965d5f6422
Massimiliano Arione <garakkio@gmail.com>
2019-11-27 10:32:42 +0100

    migliora terminologia nei test

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -11,31 +11,31 @@ final class GildedRoseTest extends TestCase
     /**
      * @dataProvider provider
      */
-    public function testItems(Item $item, int $expected, int $sellIn): void
+    public function testItems(Item $item, int $expectedQuality, int $days): void
     {
         $rose = new GildedRose([$item]);
 
-        while ($sellIn > 1) {
+        while ($days > 1) {
             $rose->updateQuality();
-            --$sellIn;
+            --$days;
         }
 
-        $this->assertEquals($expected, $item->quality);
+        $this->assertEquals($expectedQuality, $item->quality);
     }
 
     public function provider(): array
     {
         return [
-            'foo' => ['item' => new Item('foo', 9, 10), 'expected' => 2, 'sellIn' => 9],
-            'Aged Brie' => ['item' => new Item('Aged Brie', 9, 10), 'expected' => 18, 'sellIn' => 9],
-            'Backstage' => ['item' => new Item('Backstage passes', 9, 10), 'expected' => 30, 'sellIn' => 9],
-            'Backstage2' => ['item' => new Item('Backstage passes', 9, 10), 'expected' => 0, 'sellIn' => 19],
-            'Backstage3' => ['item' => new Item('Backstage passes', 9, 10), 'expected' => 0, 'sellIn' => 19],
-            'Sulfuras1' => ['item' => new Item('Sulfuras', 9, 10), 'expected' => 80, 'sellIn' => 9],
-            'aged-foo' => ['item' => new Item('foo', 3, 14), 'expected' => 1, 'sellIn' => 9],
-            'qualityFoo' => ['item' => new Item('foo', 3, 94), 'expected' => 39, 'sellIn' => 30],
-            'qualityBrie' => ['item' => new Item('Aged Brie', 3, 94), 'expected' => 94, 'sellIn' => 30],
-            'cheapBrie' => ['item' => new Item('Aged Brie', 3, 3), 'expected' => 50, 'sellIn' => 30],
+            'foo' => ['item' => new Item('foo', 9, 10), 'expectedQuality' => 2, 'days' => 9],
+            'Aged Brie' => ['item' => new Item('Aged Brie', 9, 10), 'expectedQuality' => 18, 'days' => 9],
+            'Backstage' => ['item' => new Item('Backstage passes', 9, 10), 'expectedQuality' => 30, 'days' => 9],
+            'Backstage2' => ['item' => new Item('Backstage passes', 9, 10), 'expectedQuality' => 0, 'days' => 19],
+            'Backstage3' => ['item' => new Item('Backstage passes', 9, 10), 'expectedQuality' => 0, 'days' => 19],
+            'Sulfuras1' => ['item' => new Item('Sulfuras', 9, 10), 'expectedQuality' => 80, 'days' => 9],
+            'aged-foo' => ['item' => new Item('foo', 3, 14), 'expectedQuality' => 1, 'days' => 9],
+            'qualityFoo' => ['item' => new Item('foo', 3, 94), 'expectedQuality' => 39, 'days' => 30],
+            'qualityBrie' => ['item' => new Item('Aged Brie', 3, 94), 'expectedQuality' => 94, 'days' => 30],
+            'cheapBrie' => ['item' => new Item('Aged Brie', 3, 3), 'expectedQuality' => 50, 'days' => 30],
         ];
     }
 }
